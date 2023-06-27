263b678abcb466986b8df1956d2344b0f6ce9808
emilycxt <emilycxt2015@gmail.com>
2023-06-27 12:37:49 -0300

    içamento

---
+++ conceitos-basicosII/8hoisting/app.js
@@ -0,0 +1,7 @@
+// var nome; //iça a declaração da variável
+
+console.log (nome);// undefined - aqui ocorre o hoisting (içamento)
+
+var nome = 'Geek'; // declarando e inicializando a variável
+
+console.log (nome);
